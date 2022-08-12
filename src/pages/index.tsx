24074eca7f6243cb89ee569b0c696b63733b1056
Hunter <securtyrisk@gmail.com>
2022-08-11 23:52:23 -0400

    format

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,13 @@ import type { NextPage } from "next";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import { trpc } from "../utils/trpc";
-import { FaCopy, FaDiscord, FaGoogle, FaSignOutAlt, FaTwitch } from "react-icons/fa";
+import {
+  FaCopy,
+  FaDiscord,
+  FaGoogle,
+  FaSignOutAlt,
+  FaTwitch,
+} from "react-icons/fa";
 import Image from "next/image";
 
 const copyUrlToClipboard = (path: string) => () => {
